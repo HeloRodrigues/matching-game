74bfa0272387b6679b45764c51d9f5700a4bea66
HeloRodrigues <haraurelio@gmail.com>
2022-09-22 15:38:54 +0200

    Create BoardGame object without CSS

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import "./src/styles/settings/colors.css";
 import "./src/styles/generic/reset.css"
 import "./src/styles/elements/base.css";
-import CardGame from "./src/components/CardGame";
+import BoardGame from "./src/objects/boardGame";
 
 
 const root = document.querySelector("#root");
 
-const htmlCardGame = CardGame();
-console.log(htmlCardGame);
+const htmlBoardGame = BoardGame(6);
 
-root.insertAdjacentHTML("beforeend", htmlCardGame);
+
+root.insertAdjacentHTML("beforeend", htmlBoardGame);
