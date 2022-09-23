b0dcafa803771b5b465bf9fa44ba15607378370b
HeloRodrigues <haraurelio@gmail.com>
2022-09-23 13:21:58 +0200

    Add/Update UI and style files.
    Create ScoreBoard object.

---
+++ src/objects/ScoreBoard/index.js
@@ -0,0 +1,13 @@
+import PlayerName from "../../components/PlayerName"
+
+const ScoreBoard = () => {
+     return /*html*/ `
+      <header class= "score-board">
+          ${PlayerName(1)}
+          ${PlayerName(2)}
+      </header>
+    
+    `
+}
+
+export default ScoreBoard
