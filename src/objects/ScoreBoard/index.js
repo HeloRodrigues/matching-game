f09473b1eeea5c60017ffc4a7ae8fb3889fb007e
HeloRodrigues <haraurelio@gmail.com>
2022-10-12 11:18:31 +0200

    Update ScoreBoard.js and colors.css files

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,10 +1,12 @@
 import "./style.css"
 import PlayerName from "../../components/PlayerName"
+import VersusPlayer from "../../components/VersusPlayer"
 
 const ScoreBoard = () => {
      return /*html*/ `
       <header class= "score-board">
           ${PlayerName(1)}
+          ${VersusPlayer()}
           ${PlayerName(2)}
       </header>
     
