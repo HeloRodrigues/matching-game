27b9f93bf9354553dbf22435a77dd196beeea440
HeloRodrigues <haraurelio@gmail.com>
2022-10-16 18:27:51 +0200

    Create style.css for ArrowDown component

--- src/components/ArrowDown/index.js
+++ src/components/ArrowDown/index.js
@@ -1,6 +1,10 @@
-const ArrowDown = () => {
+import "./style.css"
+
+const ArrowDown = (currentPlayer = 1) => {
   return /*html*/ `
-    <img src = "src/images/IconArrowDown.png">
+    <img class = "arrow-down"
+         data-currentPlayer = "${currentPlayer}" 
+         src = "src/images/IconArrowDown.png">
   `
 }
 
