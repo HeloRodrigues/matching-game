e00008e4efc37a2ed54835c4d91714713d90d815
HeloRodrigues <haraurelio@gmail.com>
2022-10-17 11:33:18 +0200

    Swap player when 2 cards flip

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,13 +1,13 @@
-import "./style.css"
-import PlayerName from "../../components/PlayerName"
-import VersusPlayer from "../../components/VersusPlayer"
-import PlayerScore from "../../components/PlayerScore"
-import ArrowDown from "../../components/ArrowDown"
+import "./style.css";
+import PlayerName from "../../components/PlayerName";
+import VersusPlayer from "../../components/VersusPlayer";
+import PlayerScore from "../../components/PlayerScore";
+import ArrowDown from "../../components/ArrowDown";
 
 const ScoreBoard = () => {
-     return /*html*/ `
+  return /*html*/ `
       <header class= "score-board">
-          ${ArrowDown()}
+          ${ArrowDown(2)}
           ${PlayerName(1)}
           ${PlayerScore(2)}
           ${VersusPlayer()}
@@ -15,7 +15,7 @@ const ScoreBoard = () => {
           ${PlayerName(2)}
       </header>
     
-    `
-}
+    `;
+};
 
-export default ScoreBoard
+export default ScoreBoard;
